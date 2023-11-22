a1c30163a530baab2dcf5010aeaedeb729df8e53
Martin Brettschneider <martin.brettschneider@adbros.cz>
2023-11-22 16:09:30 +0100

    feat: add php 5.6

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-php_versions=("7.4" "8.0" "8.1" "8.2")
+php_versions=("5.6" "7.4" "8.0" "8.1" "8.2")
 
 for php_version in ${php_versions[@]}; do
   image_tag=martenb/gitlab-vshosting-ci:$php_version
-  docker build --build-arg PHP_VERSION=$php_version -t $image_tag .
+
+  if [ $php_version = "5.6" ]
+  then
+    docker build --build-arg PHP_VERSION=$php_version -t $image_tag ./deprecated
+  else
+    docker build --build-arg PHP_VERSION=$php_version -t $image_tag .
+  fi
+
   docker push martenb/gitlab-vshosting-ci:$php_version
 done
