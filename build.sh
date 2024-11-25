2313fb83a3899a33f904c0c0fd9d58bdb98e2154
Martin Brettschneider <martin.brettschneider@adbros.cz>
2024-11-25 14:42:57 +0100

    feat: add php 8.3 and 8.4

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-php_versions=("5.6" "7.4" "8.0" "8.1" "8.2")
+php_versions=("5.6" "7.4" "8.0" "8.1" "8.2" "8.3" "8.4")
 
 for php_version in ${php_versions[@]}; do
   image_tag=martenb/gitlab-vshosting-ci:$php_version
